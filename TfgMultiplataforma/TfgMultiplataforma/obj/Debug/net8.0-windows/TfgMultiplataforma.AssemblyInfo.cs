2ad30c761438a0ccbce30db4ae2bdb1f7cc92b42
pauladoradopuertas <127188150+pauladoradopuertas@users.noreply.github.com>
2025-04-03 17:00:21 +0200

    cambios

--- TfgMultiplataforma/TfgMultiplataforma/obj/Debug/net8.0-windows/TfgMultiplataforma.AssemblyInfo.cs
+++ TfgMultiplataforma/TfgMultiplataforma/obj/Debug/net8.0-windows/TfgMultiplataforma.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TfgMultiplataforma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499edef6d0908ac8ef4310924eaf49a4acd8a0cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1000a7fbd9d4bbf29081840f40240d867d45b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TfgMultiplataforma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TfgMultiplataforma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
